58633bfe18484f4434294b0295b8fce76d5cf415
AbdelrahmanMaherTera <151785642+AbdelrahmanMaherTera@users.noreply.github.com>
2023-11-24 00:38:36 +0200

    Updating

---
+++ Product_1-script.js
@@ -0,0 +1,15 @@
+var mainImg = document.getElementById("main-img");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function function1(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function function2(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function function3(){
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function function4(){
+    mainImg.src = smallImg[3].src;
+}
