5d9b8f6d06adfd19d3b268f134088f85be6f0f62
AbdelrahmanMaherTera <151785642+AbdelrahmanMaherTera@users.noreply.github.com>
2023-12-20 17:01:50 +0200

    Update Drop-down-menu.js

--- JAVASCRIPT/Drop-down-menu.js
+++ JAVASCRIPT/Drop-down-menu.js
@@ -1,7 +1,19 @@
 var subMenu = document.getElementById("sub-menu");
-var iconMenu = document.getElementById("menu-icon");
+//var iconMenu = document.getElementById("menu-icon");
 var icon = document.getElementById("icon");
 
 function dropDownMenu() {
-    subMenu.classList.toggle("open-menu");
+    //subMenu.classList.toggle("open-menu");
+    //icon.classList.toggle("active-icon");
+    var check = subMenu.classList.contains("open-menu");
+    if(check) {
+        subMenu.classList.remove("open-menu");
+        icon.style.color = '';
+        icon.style.transform = '';
+
+    } else {
+        icon.style.color = '#088178';
+        icon.style.transform = 'scale(1.2)';
+        subMenu.classList.add("open-menu");
+    }
 }
