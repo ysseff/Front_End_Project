92ef341a54ac2ca38e8454ea03045e6841aae545
Hussein-123 <hm5881831@gmail.com>
2023-12-20 18:54:54 +0200

    n

--- JAVASCRIPT/Drop-down-menu.js
+++ JAVASCRIPT/Drop-down-menu.js
@@ -1,19 +1,15 @@
 var subMenu = document.getElementById("sub-menu");
-//var iconMenu = document.getElementById("menu-icon");
 var icon = document.getElementById("icon");
 
 function dropDownMenu() {
-    //subMenu.classList.toggle("open-menu");
-    //icon.classList.toggle("active-icon");
-    var check = subMenu.classList.contains("open-menu");
-    if(check) {
-        subMenu.classList.remove("open-menu");
-        icon.style.color = '';
-        icon.style.transform = '';
-
-    } else {
-        icon.style.color = '#088178';
-        icon.style.transform = 'scale(1.2)';
-        subMenu.classList.add("open-menu");
-    }
+  var check = subMenu.classList.contains("open-menu");
+  if (check) {
+    subMenu.classList.remove("open-menu");
+    icon.style.color = "";
+    icon.style.transform = "";
+  } else {
+    icon.style.color = "#088178";
+    icon.style.transform = "scale(1.2)";
+    subMenu.classList.add("open-menu");
+  }
 }
